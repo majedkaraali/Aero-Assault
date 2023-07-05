7625e52e482a81fb376dc24bf0531c9c276c8ae0
Majed <majedkaraali@gmail.com>
2023-07-05 18:40:26 +0300

    Update

--- game.py
+++ game.py
@@ -3,8 +3,25 @@ import pygame
 pygame.init()
 WIDTH=1280
 HEIGHT=677
+score=0
 win=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Hunt")
+
+class Bullet:
+    width = 3
+    height = 7
+    vel_y = -10
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_bullet(self):
+        self.y += self.vel_y
+
+    def draw_bullet(self):
+        pygame.draw.rect(win, ('black'), (self.x, self.y, self.width, self.height))
+
 
 class Player:
     x=350
@@ -12,10 +29,12 @@ class Player:
     width=50
     height=50
     player_alive=True
-    score=0
     vel_x = 0
     vel_y = 0
     move_speed = 6
+    bullets = []
+    shoot_delay = 100  
+    last_shot_time = 0
 
     def move_player(self):
         if keys[pygame.K_LEFT]:
@@ -31,18 +50,53 @@ class Player:
     def update_player(self):
         pl = pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, self.width, self.height))
 
+    def can_shoot(self):
+        current_time = pygame.time.get_ticks()
+        return current_time - self.last_shot_time >= self.shoot_delay
+
+    def shoot(self):
+        if self.can_shoot():
+            bullet = Bullet(self.x + self.width // 2 - Bullet.width // 2, self.y)
+            self.bullets.append(bullet)
+            self.last_shot_time = pygame.time.get_ticks()
+
+    def move_bullets(self):
+        for bullet in self.bullets:
+            bullet.move_bullet()
+
+    def update_bullets(self):
+        for bullet in self.bullets:
+            bullet.draw_bullet()
 
 
 
 class Enemy:
-    x=300
-    y=10
-    width=50
-    height=50
+    def __init__(self,x,y,width,height,vel,move_dir):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+        self.vel=vel
+        self.move_dir=move_dir
+
     def set_x(self,x):
         self.x=x
     def move_enemy(self):
-        self.y+=2
+        
+
+        if self.move_dir=='right':
+            if self.x<WIDTH-60:
+                self.x+=self.vel
+            else:
+                enemy_lsit.remove(enemy)
+
+        elif self.move_dir=="left":
+            if self.x>60:
+                self.x+=self.vel
+            else:
+                enemy_lsit.remove(enemy)
+
+
     def update_enemy(self):
         en = pygame.draw.rect(win, (29, 84, 158), (self.x, self.y, self.width, self.height))
 
@@ -55,14 +109,27 @@ clock = pygame.time.Clock()
 pygame.font.init()
 pygame.mixer.init()
 
-for i in range(5):
-    enemy=Enemy()
-    enemy.set_x(random.randint(1,WIDTH))
-    enemy_lsit.append(enemy)
+
 
 
 def generate_enemies():
-    pass
+    if len(enemy_lsit)<2:
+        move_dircton=random.randint(0,1)
+        if move_dircton==1:
+            vel=2
+            x=random.randint(-350,-50)
+            mdir='right'
+        else:
+            vel=-2
+            x=random.randint(WIDTH+50,WIDTH+350)
+            mdir='left'
+        
+        enemy=Enemy(x,10,50,50,vel,mdir)
+        enemy_lsit.append(enemy)
+    else:
+        print("A")
+
+   
 
 
 
@@ -78,12 +145,17 @@ while run:
 
     p1.move_player()
     p1.update_player()
+    p1.move_bullets() 
+    p1.update_bullets() 
+    generate_enemies()
 
 
-
+    if keys[pygame.K_SPACE]:
+        p1.shoot()
     for enemy in enemy_lsit:
         enemy.move_enemy()
         enemy.update_enemy()
+        
 
     pygame.display.update()
 
