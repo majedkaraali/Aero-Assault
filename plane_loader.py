84af51bdbb78e7fc5c69c968d28d38adb9a36d2a
Majed <majedkaraali@gmail.com>
2023-07-24 21:46:12 +0300

    update

---
+++ plane_loader.py
@@ -0,0 +1,11 @@
+import pygame
+bomber_list=[]
+a7_left_1=pygame.image.load('src/img/a7-left.png')
+a7_rigt_1=pygame.image.load('src/img/a7-rigt.png')
+a7_left_2=pygame.image.load('src/img/a7-left2.png')
+a7_rigt_2=pygame.image.load('src/img/a7-rigt2.png')
+
+
+f16_left=pygame.image.load('src/img/f16-left.png')
+f16_right=pygame.image.load('src/img/f16-right.png')
+
