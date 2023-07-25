2ebcc55fd4df27c1b236d9d0f9526a6d957c5f28
Majed <majedkaraali@gmail.com>
2023-07-25 11:53:52 +0300

    update

--- plane_loader.py
+++ plane_loader.py
@@ -1,11 +1,40 @@
 import pygame
+
+
 bomber_list=[]
+drone_list=[]
+fighter_list=[]
+strike_list=[]
+
+
+
 a7_left_1=pygame.image.load('src/img/a7-left.png')
 a7_rigt_1=pygame.image.load('src/img/a7-rigt.png')
 a7_left_2=pygame.image.load('src/img/a7-left2.png')
 a7_rigt_2=pygame.image.load('src/img/a7-rigt2.png')
 
+drone_left=pygame.image.load('src/img/drone-left.png')
+drone_right=pygame.image.load('src/img/drone-right.png')
 
 f16_left=pygame.image.load('src/img/f16-left.png')
 f16_right=pygame.image.load('src/img/f16-right.png')
 
+j20_left=pygame.image.load('src/img/j20-left.png')
+j20_right=pygame.image.load('src/img/j20-right.png')
+j20_left2=pygame.image.load('src/img/j20-left2.png')
+j20_right2=pygame.image.load('src/img/j20-right2.png')
+
+mig21_left=pygame.image.load('src/img/mig21-left.png')
+mig21_right=pygame.image.load('src/img/mig21-right.png')
+
+su25_left=pygame.image.load('src/img/su25-left.png')
+su25_right=pygame.image.load('src/img/su25-right.png')
+su25_left2=pygame.image.load('src/img/su25-left2.png')
+su25_right2=pygame.image.load('src/img/su25-right2.png')
+su25_left3=pygame.image.load('src/img/su25-left3.png')
+su25_right3=pygame.image.load('src/img/su25-right3.png')
+
+su27_left=pygame.image.load('src/img/su27-left.png')
+su27_right=pygame.image.load('src/img/su27-right.png')
+su27_left2=pygame.image.load('src/img/su27-left2.png')
+su27_right2=pygame.image.load('src/img/su27-right2.png')
